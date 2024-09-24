8c76c2883aab49b3133dc207b1f9049b09dc1a77
Jonathan Mulamba <166436989+Jono-M007@users.noreply.github.com>
2024-09-24 16:54:12 +0200

    Add files via upload

--- Project 3 - Basketball free throw analysis.R
+++ Project 3 - Basketball free throw analysis.R
@@ -5,6 +5,20 @@ Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","201
 #Players
 Players <- c("KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade")
 
+#Games 
+KobeBryant_G <- c(80,77,82,82,73,82,58,78,6,35)
+JoeJohnson_G <- c(82,57,82,79,76,72,60,72,79,80)
+LeBronJames_G <- c(79,78,75,81,76,79,62,76,77,69)
+CarmeloAnthony_G <- c(80,65,77,66,69,77,55,67,77,40)
+DwightHoward_G <- c(82,82,82,79,82,78,54,76,71,41)
+ChrisBosh_G <- c(70,69,67,77,70,77,57,74,79,44)
+ChrisPaul_G <- c(78,64,80,78,45,80,60,70,62,82)
+KevinDurant_G <- c(35,35,80,74,82,78,66,81,81,27)
+DerrickRose_G <- c(40,40,40,81,78,81,39,0,10,51)
+DwayneWade_G <- c(75,51,51,79,77,76,49,69,54,62)
+
+
+
 #Free Throws
 KobeBryant_FT <- c(696,667,623,483,439,483,381,525,18,196)
 JoeJohnson_FT <- c(261,235,316,299,220,195,158,132,159,141)
@@ -17,7 +31,7 @@ KevinDurant_FT <- c(209,209,391,452,756,594,431,679,703,146)
 DerrickRose_FT <- c(146,146,146,197,259,476,194,0,27,152)
 DwayneWade_FT <- c(629,432,354,590,534,494,235,308,189,284)
 
-#Matrix
+#Matrix for freethrows
 Freethrow <- rbind(KobeBryant_FT,JoeJohnson_FT,LeBronJames_FT, CarmeloAnthony_FT,
                    DwightHoward_FT, ChrisBosh_FT, ChrisPaul_FT,
                    KevinDurant_FT, DerrickRose_FT, DwayneWade_FT)
@@ -57,6 +71,12 @@ colnames(FreeThrowAttempts) <- Seasons
 rownames(FreeThrowAttempts) <- Players
 
 FreeThrowAttempts
+
+# Matrix for number of game appearances
+Games <- rbind(KobeBryant_G, JoeJohnson_G, LeBronJames_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, ChrisPaul_G, KevinDurant_G, DerrickRose_G, DwayneWade_G)
+rm(KobeBryant_G, JoeJohnson_G, CarmeloAnthony_G, DwightHoward_G, ChrisBosh_G, LeBronJames_G, ChrisPaul_G, DerrickRose_G, DwayneWade_G, KevinDurant_G)
+colnames(Games) <- Seasons
+rownames(Games) <- Players
 
 
 #---Exploratory data analysis
